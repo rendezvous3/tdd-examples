0d774edc0e1314cf226f2988f171159f3d640f11
Bojan Jovanovic <bojanj6m@gmail.com>
2020-04-28 18:05:39 -0400

    tdd redux action fn

---
+++ src/actions/index.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import { correctGuess, actionTypes } from './';
+
+describe('correctGuess', () => {
+  test("returns an action with type `CORRECT_GUESS`", () => {
+    const action = correctGuess();
+    // toEqual is deep equality test
+    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+  });
+});
