05ddc88c2ab5c06d346c37c193ad0fa38b437f0a
Bojan Jovanovic <bojanj6m@gmail.com>
2020-04-29 17:52:25 -0400

    testing Input

--- src/__tests__/Input.test.js
+++ src/__tests__/Input.test.js
@@ -1,48 +1,63 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import React from "react";
+import { shallow, ShallowWrapper } from "enzyme";
 
-import { findByTestAttr, storeFactory } from '../../test/testUtils';
-import Input from '../components/Input';
-
+import { findByTestAttr, storeFactory } from "../../test/testUtils";
+import Input from "../components/Input";
 
 /**
  * @function - setup
  * @param {object} - initialState
  * @returns {ShallowWrapper}
  */
-const setup = (initialState={}) => {
+const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />).dive().dive();
-  console.log(wrapper.debug());
-}
+  const wrapper = shallow(<Input store={store} />)
+    .dive()
+    .dive();
+  // console.log(wrapper.debug());
+  return wrapper;
+};
 
 setup();
 
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("renders without an error", () => {
-
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("renders component without an error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     });
     test("renders input box", () => {
-
+      const component = findByTestAttr(wrapper, "input-box");
+      expect(component.length).toBe(1);
     });
     test("renders submit button", () => {
-
+      const component = findByTestAttr(wrapper, "submit-button");
+      expect(component.length).toBe(1);
     });
   });
-  describe("word has not been guessed", () => {
+  describe("word has been guessed", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup({ success: true });
+    });
     test("renders without an error", () => {
-
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     });
     test("does not render input box", () => {
-
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
     });
     test("does not render submit button", () => {
-
+      const submitBTN = findByTestAttr(wrapper, "submit-button");
+      expect(submitBTN.length).toBe(0);
     });
   });
 });
-describe("update state", () => {
-
-});
+describe("update state", () => {});
--- src/components/Input.js
+++ src/components/Input.js
@@ -7,16 +7,29 @@ import { connect } from "react-redux";
 //   }
 // }
 
-function Input() {
-  return (
-    <div>
-      <button type="button">Btn</button>
-    </div>
+function Input({ success }) {
+  const content = success ? null : (
+    <form className="form-inline">
+      <input
+        data-test="input-box"
+        className="mb-2 mx-sm-3"
+        type="text"
+        placeholde="enter guess"
+      />
+      <button
+        data-test="submit-button"
+        className="btn btn-primary"
+        type="submit"
+      >
+        Guess
+      </button>
+    </form>
   );
+  return <div data-test="component-input">{content}</div>;
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
