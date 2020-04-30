4358ae67bb788d5447de176598f2e79c780a909c
Bojan Jovanovic <bojanj6m@gmail.com>
2020-04-30 17:06:42 -0400

    tdd guessWord Action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
+import { getLetterMatchCount } from "../helpers";
+
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
+  GUESS_WORD: "GUESS_WORD",
 };
 
 /**
@@ -11,3 +14,26 @@ export function correctGuess() {
     type: actionTypes.CORRECT_GUESS,
   };
 }
+
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD
+ * action and (conditionaly) CORRECT_GUESS action
+ * @function - guessWord
+ * @param {string} - guessedWord
+ * @returns {function} - Redux Thunk function.
+ */
+export const guessWord = (guessedWord) => {
+  return function (dispatch, getState) {
+    const { secretWord } = getState();
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch(correctGuess());
+    }
+  };
+};
